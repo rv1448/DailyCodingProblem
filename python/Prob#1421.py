802a45e2733fff18111f447960eb91cd3e08e3cd
Rahul Vangala[sada.com] <r.reddy1448@gmail.com>
2024-02-20 22:26:27 +0000

    Changes

--- python/Prob#1421.py
+++ python/Prob#1421.py
@@ -10,4 +10,22 @@ input = [1, 2, 3, 4, 5]
 # output = [120, 60, 40, 30, 24]
 test= reduce(lambda x, y: x * y, input)
 final_output = [int(test/i) for i in input]
-print(final_output)
+
+def head_tail(arr, i):
+    len_arr = len(arr)
+    head = [1]
+
+    if i >= len_arr:
+        return [1] 
+    if i == 0:
+        return (head, arr[1::])
+    else:
+        return (arr[0:i], arr[i::len_arr])
+     
+def reduce_head_tail(test):
+    head, tail = test
+    return reduce(lambda x,y: x *y, head) * reduce(lambda x,y: x *y, tail)
+
+print(head_tail([10,2,3,4,5,6],1))
+tests = head_tail([10,2,3,4,5,6],0)
+print(reduce_head_tail(tests))
